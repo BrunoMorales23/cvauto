c8683062c245a93f3ed0343e18ec39eefd906173
Bruno Morales <brunomorales0323@outlook.es>
2025-06-13 13:08:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,88 +1,151 @@
-import pdfplumber
-from pathlib import Path
-from dotenv import load_dotenv
-import google.generativeai as genai
-import os, time
+Public finalws As String
+Public codJurisdiccion As String
+Public coeficienteUnificadoDistribucion As Variant
+Public baseImpo As String
+Public outputPath As String
+Public total As String
+Public codAct As String
+Public totalColumns As Integer
+Public mainInitialRow As Integer
+Public alicuota As String
 
-texto = ""
-load_dotenv()
-api_key = os.getenv("API_KEY")
+Sub MostrarFormulario()
+    UserForm1.Show
+    LoopUntilNoResult
+End Sub
 
-
-input_value = input("Como proceder. 1 PUSH | 2 Modify ...  ")
-
-if input_value == "1":
-
-    root_path = Path("C:/Users/bmorales/OneDrive - rmrconsultores.com/Escritorio/cv")
-    pdf_paths = list(root_path.glob('*.pdf'))
-
-
-    for path in pdf_paths:
-        with pdfplumber.open(path) as pdf:
-            for pagina in pdf.pages:
-                texto += pagina.extract_text()
-        print("Success")
-
-    genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('models/learnlm-2.0-flash-experimental')
-
-    prompt = f"""Filtra los nombres de las personas, a quienes estos curriculums vitae pertenece. Posicionalos junto a su correo electrónico. Ten en cuenta la separación de guiones, que define distintos contextos." \
-    lo que debes analizar, es el siguiente texto: {texto}
-    Evita generar oraciones tal como 'De acuerdo, aquí están los nombres y correos electrónicos filtrados de los currículums', '¡Entendido! Aquí está la lista filtrada con el cambio solicitado:', 'Okay, I have the following two email addresses:' y enfocate en solo devolver el resultado
-    Corta la primer oración generada y solo devuelve los resultados 'Nombre' | 'Correo'
-    Extrae únicamente la información de nombres y correos electrónicos tal como aparece en el texto. No agregues ninguna explicación, conclusión ni interpretación adicional. El formato de salida debe ser exactamente este:
-INSTRUCCIONES ESTRICTAS:
-
-Extrae solamente líneas que contengan nombres completos seguidos de su correo electrónico, exactamente en este formato:
-
-* Nombre Apellido - correo@ejemplo.com
-
-NO agregues:
-- Frases como “okay”, “here is”, “I have extracted”, etc.
-- Títulos, encabezados o frases de cierre.
-- Ninguna palabra fuera del formato indicado.
-
-🔴 Si escribes **algo fuera del formato exacto**, se considerará un ERROR.
-
-Si no hay coincidencias, tu respuesta debe estar completamente vacía.
-
-
-    """
-
-    response = model.generate_content(prompt)
-    print(response.text)
-
-elif input_value == "2":
-    order_value = input("Como proceder. 1 Pisar Prompt | 2 Complementar Prompt")
-    if order_value == "1":
-        prompt = input("Deposite el nuevo prompt: ")
-    elif order_value == "2":
-        new_prompt = input("Deposite la nueva instrucción para el prompt: ")
-
-        root_path = Path("C:/Users/bmorales/OneDrive - rmrconsultores.com/Escritorio/cv")
-        pdf_paths = list(root_path.glob('*.pdf'))
-
-
-        for path in pdf_paths:
-            with pdfplumber.open(path) as pdf:
-                for pagina in pdf.pages:
-                    texto += pagina.extract_text()
-
-        genai.configure(api_key=api_key)
-        model = genai.GenerativeModel('models/learnlm-2.0-flash-experimental')
-
-        prompt = f"""Filtra los nombres de las personas, a quienes estos curriculums vitae pertenece. Posicionalos junto a su correo electrónico. Ten en cuenta la separación de guiones, que define distintos contextos." \
-        lo que debes analizar, es el siguiente texto: {texto} PORFAVOR, DEVUELVE SOLAMENTE LO SOLICITADO, NO REQUIERO DE NINGUN SCRIPT, NINGUNA EXPLICACION NI NINGUNA DEFINICION, SOLAMENTE SE REQUIERE OBTENER LO CONSULTADO EN EL PROMPT
-        """
-
-        prompt_instruction = f"Teniendo en cuenta el prompt inicial, osea: {prompt}, y el nuevo prompt recibido: {new_prompt}. Es necesario que fusiones ambas instrucciones, en un solo prompt, optimizandolo lo más posible a fin de utilizar la menor cantidad de tokens posibles. PORFAVOR, DEVUELVE SOLAMENTE LO SOLICITADO, NO REQUIERO DE NINGUN SCRIPT, NINGUNA EXPLICACION NI NINGUNA DEFINICION, SOLAMENTE SE REQUIERE OBTENER LO CONSULTADO EN EL PROMPT. CUT ANY SENTENCE THAT IS NOT REFERED TO THE REQUERIMENT ITSELF"
-
-        new_prompt = prompt_instruction
-
-        response = model.generate_content(new_prompt)
-        prompt = response.text
-        time.sleep(5)
-        response = model.generate_content(prompt)
-        print(response.text)
-
+Sub LoopUntilNoResult()
+    Dim currentCell As Range
+    Dim currentRow As Integer
+    Dim currentColumn As Integer
+    Dim currentSheet As String
+    Dim wbOrigen As Workbook
+    Dim wbOrigenDir As String
     
+    Dim wsOrigen As Worksheet
+    currentRow = 5
+    
+    Dim outputIncremental As Integer
+    outputIncremental = 12
+    
+    Dim outputWs As Worksheet
+    
+    Dim ultimaColumna As Long, j As Long
+    Dim codAdc As String
+    
+    Set currentCell = Range("A" & currentRow)
+    
+    ' Path del archivo core
+    wbOrigenDir = ThisWorkbook.FullName
+    Set wbOrigen = Workbooks.Open(wbOrigenDir)
+    Set wsOrigen = wbOrigen.Sheets(finalws)
+    ' Path del archivo Excel output
+    'outputPath = "C:\Users\bmorales\OneDrive - rmrconsultores.com\Escritorio\AAAAAAAAAA.xlsx"
+    Set wbOutput = Workbooks.Open(outputPath)
+    
+    ' Obtenemos todo de la row 3 a modo de ancla
+    ultimaColumna = wsOrigen.Cells(3, wsOrigen.Columns.Count).End(xlToLeft).Column
+    
+    'Row inicial del primer codigo de Jurisdiccion
+    mainInitialRow = 5
+    'Column inicial del primer dato de Cod de Actividad
+    currentColumn = 4
+    
+    ' While que trabaja la operatoria en torno a la column A
+    Do While wsOrigen.Cells(mainInitialRow, 1).Value <> "" And LCase(wsOrigen.Cells(mainInitialRow, 1).Value) <> "TOTAL INGRESOS"
+    
+    ' Data que se rige por row
+    codJurisdiccion = wsOrigen.Range("A" & currentRow).Value
+    
+    If Trim(codJurisdiccion) = "" Then
+        Exit Do
+    End If
+    
+    coeficienteUnificado = wsOrigen.Range("C" & currentRow).Value
+    total = wsOrigen.Range("S" & currentRow).Value
+    
+    Worksheets("Template").Copy After:=Worksheets(Worksheets.Count)
+    ActiveSheet.Name = codJurisdiccion
+    currentSheet = ActiveSheet.Name
+    
+    ' Data que se rige por column
+    ' Trabajamos lo obtenido en la row 3
+    For j = 1 To ultimaColumna
+        If InStr(1, wsOrigen.Cells(3, j).Value, "Serv", vbTextCompare) > 0 Then
+            codAct = Trim(Split(wsOrigen.Cells(3, j).Value, "-")(0))
+            
+            
+            ModifyData outputIncremental, currentSheet, codAct
+            
+            baseImpo = wsOrigen.Cells(currentRow, currentColumn).Value
+            currentColumn = currentColumn + 1
+            alicuota = wsOrigen.Cells(currentRow, currentColumn).Value * 100
+                    
+            AddColumnData outputIncremental, baseImpo, alicuota, currentSheet
+            AddFinalData total, coeficienteUnificado, currentSheet
+            
+            
+            outputIncremental = outputIncremental + 1
+            currentColumn = currentColumn + 2
+        End If
+    Next j
+    
+    'Reset Values
+    currentRow = currentRow + 1
+    currentColumn = 4
+    outputIncremental = 12
+    
+    'MsgBox "Finalizado Sheet para: " & codJurisdiccion
+    
+    Loop
+    MsgBox "Operatoria Finalizada Exitosamente."
+End Sub
+
+Sub ModifyData(outputIncremental As Integer, currentWs As String, codAct As String)
+'Se encarga de aplicar datos nulos en la tabla y el código de actividad
+
+    Dim ws As Worksheet
+    Set ws = Worksheets(currentWs)
+    Dim wb As Workbook
+    Set wb = ThisWorkbook
+    
+    ws.Range("B" & outputIncremental).Value = "0"
+    ws.Range("C" & outputIncremental).Value = codAct
+    ws.Range("D" & outputIncremental).Value = "0"
+    ws.Range("F" & outputIncremental).Value = "0"
+    
+    ' Guardar cambios
+    wb.Save
+    ' wb.Close
+    
+End Sub
+
+Sub AddFinalData(total As String, coeficienteUnificado As Variant, currentWs As String)
+'Se encarga de aplicar los datos que no tienen repetición
+
+    Dim ws As Worksheet
+    Set ws = Worksheets(currentWs)
+    Dim wb As Workbook
+    Set wb = ThisWorkbook
+    
+    ws.Range("C3").Value = CDbl(coeficienteUnificado)
+    ws.Range("C6").Value = CDbl(total)
+    
+    wb.Save
+
+End Sub
+
+Sub AddColumnData(outputIncremental As Integer, baseImpo As String, alicuota As String, currentWs As String)
+'Se encarga de poner los datos con repetición
+
+    Dim ws As Worksheet
+    Set ws = Worksheets(currentWs)
+    Dim wb As Workbook
+    Set wb = ThisWorkbook
+    
+    ws.Range("E" & outputIncremental).Value = baseImpo
+    ws.Range("G" & outputIncremental).Value = alicuota
+    
+    wb.Save
+
+End Sub
